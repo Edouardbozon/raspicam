08cdea6db1d77dacd844efd04c46eac63e300304
edouardbozon <bozon.edouard@live.fr>
2016-07-27 08:11:19 +0200

    Add eslint, babel, nodemon for a better workflow

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "parserOptions": {
+        "parser": "babel-eslint",
+      "sourceType": "module",
+      "ecmaVersion": 6
+  },
+  "env": {
+     "node": true,
+     "es6": true
+  },
+  "rules":{
+     "vars-on-top": 2,
+     "no-undef": 2
+  }
+};
